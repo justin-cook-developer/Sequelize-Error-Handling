b62e03438101e13031e5ab809d695ce2e2c95475
Justin Cook <justin.cook.dev@gmail.com>
2019-07-02 16:25:17 -0700

    Completed initial tests for User model.

--- tests/models/users_model.test.js
+++ tests/models/users_model.test.js
@@ -2,9 +2,9 @@
 
 const chai = require('chai');
 const expect = chai.expect;
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
-chai.use(sinonChai);
+// const sinon = require('sinon');
+// const sinonChai = require('sinon-chai');
+// chai.use(sinonChai);
 
 const { User, connection } = require('../../server/db/index');
 
@@ -105,6 +105,73 @@ describe('The `User` Model', () => {
 
       if (result)
         throw Error('validation should fail when email is not a valid email');
+
+      expect(error).to.be.an.instanceOf(Error);
+    });
+
+    it('requires `email` to be unique', async () => {
+      await user.save();
+
+      const user2 = User.build({
+        name: 'Jeff SuperSteez',
+        email: 'jeffDrinksBeer@gmail.com',
+        password: 'jeffSteezTookMyEmail',
+      });
+
+      let result, error;
+      try {
+        result = await user2.validate();
+      } catch (err) {
+        error = err;
+      }
+
+      if (result) throw Error('validation should fail if email is not unique');
+
+      expect(error).to.be.an.instanceOf(Error);
+    });
+
+    it('requires password', async () => {
+      user.password = null;
+
+      let result, error;
+      try {
+        result = await user.validate();
+      } catch (err) {
+        error = err;
+      }
+
+      if (result) throw Error('validation should fail when password is null');
+
+      user.password = '';
+
+      let result2, error2;
+      try {
+        result2 = await user.validate();
+      } catch (err) {
+        error2 = err;
+      }
+
+      if (result2)
+        throw Error('validation should fail when password is an empty string');
+
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error2).to.be.an.instanceOf(Error);
+    });
+
+    it('requires password to be longer than 6 characters', async () => {
+      user.password = 'hiBro';
+
+      let result, error;
+      try {
+        result = await user.validate();
+      } catch (err) {
+        error = err;
+      }
+
+      if (result)
+        throw Error(
+          'validation should fail if password is less than 7 characters'
+        );
 
       expect(error).to.be.an.instanceOf(Error);
     });
