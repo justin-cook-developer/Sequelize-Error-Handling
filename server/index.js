b0bb128c8d1b49b178e616d8c383f1e680fc14da
Justin Cook <justin.cook.dev@gmail.com>
2019-07-03 08:48:40 -0700

    Added logging to error handler.

--- server/index.js
+++ server/index.js
@@ -15,11 +15,13 @@ app.use('/api', require('./api/index'));
 app.use((req, res, next) => {
   res
     .status(404)
-    .send('Invalid route. The only valid routes lie at /api/users.');
+    .send('Invalid route. The only valid routes lie at /api/users');
 });
 
 app.use((e, req, res, next) => {
+  console.log('my log start --------');
   console.error(e);
+  console.log('my log end ------');
   next(e);
 });
 
